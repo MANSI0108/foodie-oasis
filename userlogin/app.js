0f4f24e8c3ee753fe98c2860d8e2f70848440655
Mansi Sojitra <mansisojitra0108@gmail.com>
2024-03-29 17:16:43 +0530

    start-documentation

--- userlogin/app.js
+++ userlogin/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
 const authRoute = require('./src/routes/userRoute.js')
-
 const errorHandler = require("./src/middleware/asyncHandler.js");
-
-
+const swaggerUi = require("swagger-ui-express")
+const YAML = require("yaml")
+const fs = require("fs-extra")
 app.use(express.json())
+//Swagger For Documentation
+
+const file = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+
 app.use(express.urlencoded({ extended: true }))
-
 app.use('/foodApp', authRoute)
-
 app.use(errorHandler)
 
 module.exports = app; 
