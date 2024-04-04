04a289ecfc9f9dbe3e3f8066ed65264979ae5d7d
Mansi Sojitra <mansisojitra0108@gmail.com>
2024-04-04 10:40:47 +0530

    index.js in Routes

--- restaurant/app.js
+++ restaurant/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const ErrorHandler = require("./src/middleware/asyncHandler");
-const restaurantAuth = require("./src/routes/restaurant")
-const menu_Route = require("./src/routes/menuRoute.js");
+const Routes = require("./src/routes/index.js")
 const verifyToken = require("./src/middleware/verifyToken.js");
 const app = express();
 
@@ -17,8 +16,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.urlencoded({ extended: true }))
 app.use(verifyToken)
 
-app.use('/foodApp', restaurantAuth)
-app.use('/foodApp/menu', menu_Route)
+app.use('/foodApp', Routes)
+
 
 app.use(ErrorHandler)
 
